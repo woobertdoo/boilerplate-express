22df023e055525405310c2bcda73511b20eeb4e4
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2022-09-11 09:21:28 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,20 +1,12 @@
 let express = require('express');
 let app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + '/public/style.css');
 })
-
-
-
-
-
-
-
-
-
-
-
 
 
 
